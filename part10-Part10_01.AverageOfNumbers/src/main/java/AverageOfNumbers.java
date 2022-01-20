0b1fa55e81b9d0c4143825d247476a05c9525eab
vidyakinjarapu <vidya5594@gmail.com>
2022-01-20 12:19:21 +0530

    Add new changes

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AverageOfNumbers {
@@ -7,6 +8,22 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your program here
+        List<Integer> inputs = new ArrayList<>();
 
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+
+            inputs.add(Integer.valueOf(input));
+        }
+        
+        double average = inputs.stream()
+                .mapToInt(i -> i)
+                .average()
+                .getAsDouble();  
+        System.out.println("average of the numbers: " + average);
     }
 }
--- part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -1,12 +1,40 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
-
+        List<Integer> inputs = new ArrayList<>();
+        
+        System.out.println("Input numbers, type \"end\" to stop");
+        
+        while(true){
+            String in = scanner.nextLine();
+            if(in.equals("end")){
+                break;
+            }
+            inputs.add(Integer.valueOf(in));
+        }
+        
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        String userIp = scanner.nextLine();
+        if(userIp.equals("n")){
+            double negAverage = inputs.stream()
+                .mapToInt(i -> i)
+                .filter(i -> i < 0)
+                .average()
+                .getAsDouble();
+            System.out.println("Average of the negative numbers: " + negAverage);
+        } else {
+            double posAverage = inputs.stream()
+                .mapToInt(i -> i)
+                .filter(i -> i >= 0)
+                .average()
+                .getAsDouble();
+            System.out.println("Average of the positive numbers: " + posAverage);
+        }
     }
 }

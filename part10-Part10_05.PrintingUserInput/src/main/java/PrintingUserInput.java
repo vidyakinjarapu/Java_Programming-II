13b2777f08a67e6fe927f416a832f707a9089546
vidyakinjarapu <vidya5594@gmail.com>
2022-01-25 22:24:52 +0530

    Add excercise in part 10

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -6,6 +6,21 @@ public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<String> listItems = new ArrayList<>();
+        
+        System.out.println("Press an empty string (only presses enter) to stop : ");
+        while(true){
+            String ip = scanner.nextLine();
+            if(ip.equals("")){
+                break;
+            }
+            listItems.add(ip);
+        }
+        
+//        String finalip = listItems.stream()
+//                .reduce("", (added, item)-> added + item + "\n");
+//        System.out.println(finalip);
+        
+        listItems.stream().forEach(s -> System.out.println(s));
     }
 }
--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,21 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> items = new ArrayList<>();
+        
+        System.out.println("Giving negative input will stop the loop: ");
+        while(true){
+            int num = Integer.valueOf(scanner.nextLine());
+            if(num < 0){
+                break;
+            }
+            
+            items.add(num);
+        }
+        
+        items.stream()
+                .filter(n -> n >=1 && n <=5 )
+                .forEach(item -> System.out.println(item));
 
     }
 }

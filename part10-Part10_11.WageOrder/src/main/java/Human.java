9fd3ba1976c04d36d48aa0c46f88ed5159b39337
vidyakinjarapu <vidya5594@gmail.com>
2022-01-28 12:20:42 +0530

    Learn Lambda expressions

--- part10-Part10_11.WageOrder/src/main/java/Human.java
+++ part10-Part10_11.WageOrder/src/main/java/Human.java
@@ -1,5 +1,5 @@
 
-public class Human {
+public class Human implements Comparable<Human>{
 
     private int wage;
     private String name;
@@ -18,8 +18,18 @@ public class Human {
     }
 
 
-    @Override
     public String toString() {
         return name + " " + wage;
+    }
+
+    @Override
+    public int compareTo(Human human) {
+        if(this.wage == human.getWage()){
+            return 0;
+        } else if(this.wage > human.getWage()){
+            return -1;
+        } else {
+            return 1;
+        }
     }
 }
--- part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/Student.java
+++ part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/Student.java
@@ -1,17 +1,20 @@
 
 
-public class Student {
-
+public class Student implements Comparable<Student> {
     private String name;
-
-    public Student(String name) {
+    public Student(String name){
         this.name = name;
     }
-
+    
     public String getName() {
         return name;
     }
+    @Override
+    public int compareTo(Student stu){
+//        return this.name.compareTo(stu.getName());
+        return this.name.compareToIgnoreCase(stu.getName());
 
+    }
     @Override
     public String toString() {
         return name;

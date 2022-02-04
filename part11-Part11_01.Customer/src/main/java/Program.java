b47ff2c51eb34e87970bf7bf7f62e78f47ea9d2f
vidyakinjarapu <vidya5594@gmail.com>
2022-02-04 14:23:40 +0530

    Add chnge to git

--- part11-Part11_01.Customer/src/main/java/Program.java
+++ part11-Part11_01.Customer/src/main/java/Program.java
@@ -6,6 +6,6 @@ public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // you can test your class here
-
+        System.out.println("Testing the git command line");
     }
 }

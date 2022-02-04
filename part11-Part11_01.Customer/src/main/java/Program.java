6eaef3b95ed6cfd1b40be30b7ce5df77bab690d2
vidyakinjarapu <vidya5594@gmail.com>
2022-02-04 15:17:01 +0530

    test

--- part11-Part11_01.Customer/src/main/java/Program.java
+++ part11-Part11_01.Customer/src/main/java/Program.java
@@ -6,7 +6,6 @@ public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // you can test your class here
-        System.out.println("Testing the git command line");
-        System.out.println("Add another line to check git");
+        System.out.println("Test");
     }
 }
